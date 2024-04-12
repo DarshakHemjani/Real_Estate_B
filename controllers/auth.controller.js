1e7b00f59c3be120c12b8ad23b9e1f5b7ef885c2
DarshakHemjani <dasuhemjani@gail.com>
2024-04-12 11:25:45 +0530

    authjs in change

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ import e from "express";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
  console.log("req.body", req.body);
@@ -25,12 +26,10 @@ export const signin = async (req, res, next) => {
         const validUser = await User.findOne({email: email});
         if(!validUser) return next(errorHandler(404,"User not found"));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if(!validPassword) return next(errorHandler(401,"Wrong Credentials"));
+        if(!validPassword) return next(errorHandler(401,"Wrong Credentials!"));
         const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
         
-        const userObj = validUser.toObject();
-
-        const { password: pass, ...rest } = userObj;
+        const { password: pass, ...rest } = validUser._doc;
         
         res.cookie("access_token", token, {httpOnly: true}).status(200).json(rest);
     } catch (error) {
